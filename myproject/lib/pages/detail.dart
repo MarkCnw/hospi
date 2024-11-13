18e0239ae5b95a933e6542cdf3500d27e3c83e03
MarkCnw <chinnawong554@gmail.com>
2024-11-13 21:54:02 +0700

    edit text

--- myproject/lib/pages/detail.dart
+++ myproject/lib/pages/detail.dart
@@ -4,10 +4,7 @@ import 'package:myproject/models/doctor.dart';
 
 class DetailPage extends StatefulWidget {
   final DoctorModel doctorModel;
-  const DetailPage({
-    required this.doctorModel,
-    super.key
-  });
+  const DetailPage({required this.doctorModel, super.key});
 
   @override
   State<DetailPage> createState() => _DetailPageState();
@@ -34,33 +31,21 @@ class _DetailPageState extends State<DetailPage> {
         toolbarHeight: 100,
         leadingWidth: 100,
         leading: IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-          }, 
-          icon: Container(
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(14),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xff050618).withOpacity(0.06),
-                  blurRadius: 25,
-                  offset: const Offset(0, 4)
-                )
-              ]
-            ),
-            child: const Icon(
-              Icons.arrow_back
-            ),
-          )
-        ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(14),
+                  boxShadow: [BoxShadow(color: const Color(0xff050618).withOpacity(0.06), blurRadius: 25, offset: const Offset(0, 4))]),
+              child: const Icon(Icons.arrow_back),
+            )),
         title: const Text(
           'ประวัติหมอ',
-          style: TextStyle(
-            fontSize: 18
-          ),
+          style: TextStyle(fontSize: 18),
         ),
       ),
       body: Padding(
@@ -69,9 +54,13 @@ class _DetailPageState extends State<DetailPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             info(),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             calendar(),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             time(),
             const Spacer(),
             bookButton()
@@ -92,98 +81,82 @@ class _DetailPageState extends State<DetailPage> {
               Container(
                 width: 105,
                 decoration: BoxDecoration(
-                  color: widget.doctorModel.imageBox,
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                    alignment: Alignment.bottomCenter,
-                    image: AssetImage(widget.doctorModel.image)
-                  )
-                ),
+                    color: widget.doctorModel.imageBox,
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(alignment: Alignment.bottomCenter, image: AssetImage(widget.doctorModel.image))),
               ),
               Padding(
                 padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.doctorModel.name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.doctorModel.name,
+                      style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                    ),
+                    Text(
+                      widget.doctorModel.specialties.first,
+                      style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                          size: 18,
                         ),
-                      ),
-                      Text(
-                        widget.doctorModel.specialties.first,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12
+                        const SizedBox(
+                          width: 5,
                         ),
-                      ),
-                      const Spacer(),
-                        Row(
-                          children: [
-                            const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 18,
-                              ),
-                              const SizedBox(width: 5, ),
-                                Text(
-                                  widget.doctorModel.score.toString(),
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 12
-                                  ),
-                                )
-                          ],
+                        Text(
+                          widget.doctorModel.score.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
                         )
-                    ],
-                  ),
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         const Text(
           'ผู้เชี่ยวชาญ',
-          style: TextStyle(
-            color: Color(0xff09121C),
-            fontWeight: FontWeight.w300
-          ),
+          style: TextStyle(color: Color(0xff09121C), fontWeight: FontWeight.w300),
         ),
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         Text(
           widget.doctorModel.bio,
-          style: TextStyle(
-            color: const Color(0xff09121C).withOpacity(0.5),
-            fontWeight: FontWeight.w300,
-            fontSize: 12
-          ),
+          style: TextStyle(color: const Color(0xff09121C).withOpacity(0.5), fontWeight: FontWeight.w300, fontSize: 12),
         ),
-        const SizedBox(height: 20,),
-         const Text(
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
           'Specialities',
-          style: TextStyle(
-            color: Color(0xff09121C),
-            fontWeight: FontWeight.w300
-          ),
+          style: TextStyle(color: Color(0xff09121C), fontWeight: FontWeight.w300),
         ),
-       const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         SizedBox(
           height: 50,
           child: ListView.separated(
             itemBuilder: (context, index) {
               return Text(
                 widget.doctorModel.specialties[index],
-                style: const TextStyle(
-                  color: Color(0xff09121C),
-                  decoration: TextDecoration.underline,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 12
-                ),
+                style: const TextStyle(color: Color(0xff09121C), decoration: TextDecoration.underline, fontWeight: FontWeight.w300, fontSize: 12),
               );
             },
-            separatorBuilder: (context, index) => const SizedBox(width: 10,), 
+            separatorBuilder: (context, index) => const SizedBox(
+              width: 10,
+            ),
             itemCount: widget.doctorModel.specialties.length,
             scrollDirection: Axis.horizontal,
           ),
@@ -191,78 +164,68 @@ class _DetailPageState extends State<DetailPage> {
       ],
     );
   }
-  
+
   Widget calendar() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
           'Calendar',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 16
-          ),
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
         SizedBox(
           height: 50,
           child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: (){
-                  for(var item in calendarData){
-                    item.isSelected = false;
-                  }
-                  calendarData[index].isSelected = true;
-                  setState(() {});
-                },
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: calendarData[index].isSelected ?
-                    const Color(0xff51A8FF) : Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 4),
-                        blurRadius: 25,
-                        color: calendarData[index].isSelected?
-                          const Color(0xff51A8FF).withOpacity(0.45) :
-                         const Color(0xff050618).withOpacity(0.05),
-                      )
-                    ]
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        calendarData[index].dayNumber.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: calendarData[index].isSelected ? 
-                          Colors.white : Colors.black
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    for (var item in calendarData) {
+                      item.isSelected = false;
+                    }
+                    calendarData[index].isSelected = true;
+                    setState(() {});
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: calendarData[index].isSelected ? const Color(0xff51A8FF) : Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 4),
+                            blurRadius: 25,
+                            color: calendarData[index].isSelected
+                                ? const Color(0xff51A8FF).withOpacity(0.45)
+                                : const Color(0xff050618).withOpacity(0.05),
+                          )
+                        ]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          calendarData[index].dayNumber.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 12, color: calendarData[index].isSelected ? Colors.white : Colors.black),
                         ),
-                      ),
-                      Text(
-                        calendarData[index].dayName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w200,
-                          fontSize: 12,
-                          color: calendarData[index].isSelected ? 
-                          Colors.white : Colors.black
-                        ),
-                      )
-                    ],
+                        Text(
+                          calendarData[index].dayName,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w200, fontSize: 12, color: calendarData[index].isSelected ? Colors.white : Colors.black),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }, 
-            separatorBuilder: (context, index) => const SizedBox(width: 50,),
-            itemCount: calendarData.length
-          ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                    width: 50,
+                  ),
+              itemCount: calendarData.length),
         )
       ],
     );
@@ -274,80 +237,66 @@ class _DetailPageState extends State<DetailPage> {
       children: [
         const Text(
           'Time',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 16
-          ),
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
         SizedBox(
           height: 40,
           child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: (){
-                  for(var item in timeData){
-                    item.isSelected = false;
-                  }
-                  timeData[index].isSelected = true;
-                  setState(() {});
-                },
-                child: Container(
-                  height: 40,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    color: timeData[index].isSelected ?
-                    const Color(0xff51A8FF) : Colors.white,
-                    borderRadius: BorderRadius.circular(44),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 4),
-                        blurRadius: 25,
-                        color: timeData[index].isSelected?
-                          const Color(0xff51A8FF).withOpacity(0.45) :
-                         const Color(0xff050618).withOpacity(0.05),
-                      )
-                    ]
-                  ),
-                  child: Center(
-                    child: Text(
-                      timeData[index].time,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 12,
-                        color: timeData[index].isSelected ? 
-                        Colors.white : Colors.black
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    for (var item in timeData) {
+                      item.isSelected = false;
+                    }
+                    timeData[index].isSelected = true;
+                    setState(() {});
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        color: timeData[index].isSelected ? const Color(0xff51A8FF) : Colors.white,
+                        borderRadius: BorderRadius.circular(44),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 4),
+                            blurRadius: 25,
+                            color: timeData[index].isSelected ? const Color(0xff51A8FF).withOpacity(0.45) : const Color(0xff050618).withOpacity(0.05),
+                          )
+                        ]),
+                    child: Center(
+                      child: Text(
+                        timeData[index].time,
+                        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12, color: timeData[index].isSelected ? Colors.white : Colors.black),
                       ),
                     ),
                   ),
-                ),
-              );
-            }, 
-            separatorBuilder: (context, index) => const SizedBox(width: 60,),
-            itemCount: timeData.length
-          ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                    width: 60,
+                  ),
+              itemCount: timeData.length),
         )
       ],
     );
   }
-  
+
   Widget bookButton() {
     return MaterialButton(
-      onPressed: (){},
+      onPressed: () {},
       height: 55,
       minWidth: double.infinity,
       elevation: 0,
       color: const Color(0xff51A8FF),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(50)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
       child: const Text(
-        'click here to จอง!!',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16
-        ),
+        'click here to จองคิว!!',
+        style: TextStyle(color: Colors.white, fontSize: 16),
       ),
     );
   }
